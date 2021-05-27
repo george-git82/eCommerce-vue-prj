fc896c7cadda02c0c793d1869f32fd3c632ad0fa
Abhishek Ghosh <ghosh.abhi91@gmail.com>
2021-05-27 11:23:25 +0530

    ✔ updates source code link

--- scripts/footer.js
+++ scripts/footer.js
@@ -4,7 +4,7 @@ const footerInfo = {
       name: "Bamboo Thermal Ski Coat",
       creator: {
         name: "Abhishek Ghosh",
-        link: "https://github.com/george-git82",
+        link: "https://github.com/george-git82/eCommerce-vue-prj.git",
         linkAlt: "Git repo",
       },
     };
