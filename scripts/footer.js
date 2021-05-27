e5dc4a43295e8194b086c75ce52ee626f341bb11
Abhishek Ghosh <ghosh.abhi91@gmail.com>
2021-05-27 11:26:09 +0530

    ✔ Code format updates

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,14 +1,14 @@
 const footerInfo = {
-  data() {
-    return {
-      name: "Bamboo Thermal Ski Coat",
-      creator: {
-        name: "Abhishek Ghosh",
-        link: "https://github.com/george-git82/eCommerce-vue-prj.git",
-        linkAlt: "Git repo",
-      },
-    };
-  },
+    data() {
+        return {
+            name: "Bamboo Thermal Ski Coat",
+            creator: {
+                name: "Abhishek Ghosh",
+                link: "https://github.com/george-git82/eCommerce-vue-prj.git",
+                linkAlt: "Git repo",
+            },
+        };
+    },
 };
 
 Vue.createApp(footerInfo).mount("#footerSection");
--- scripts/productList.js
+++ scripts/productList.js
@@ -1,36 +1,36 @@
 const productApp = {
-  data() {
-    return {
-      max: 130,
-      displayLabels: true,
-      productsList: [],
-      cart: [],
-      displayCart: false,
-    };
-  },
-  created() {
-    fetch("https://hplussport.com/api/products/order/price")
-      .then((resp) => resp.json())
-      .then((data) => {
-        this.productsList = data;
-      });
-  },
-  computed: {
-    filteredProducts() {
-      return this.productsList.filter((item) => item.price <= this.max);
+    data() {
+        return {
+            max: 130,
+            displayLabels: true,
+            productsList: [],
+            cart: [],
+            displayCart: false,
+        };
     },
-    cartTotal() {
-      return this.cart.reduce((inc, item) => Number(item.price) + inc, 0);
+    created() {
+        fetch("https://hplussport.com/api/products/order/price")
+            .then((resp) => resp.json())
+            .then((data) => {
+                this.productsList = data;
+            });
     },
-  },
-  methods: {
-    currency(value) {
-      let fpnNum = Number.parseFloat(value).toFixed(2);
-      return `$${fpnNum}`;
+    computed: {
+        filteredProducts() {
+            return this.productsList.filter((item) => item.price <= this.max);
+        },
+        cartTotal() {
+            return this.cart.reduce((inc, item) => Number(item.price) + inc, 0);
+        },
     },
-    addToCart(item, e) {
-      this.cart.push(item);
+    methods: {
+        currency(value) {
+            let fpnNum = Number.parseFloat(value).toFixed(2);
+            return `$${fpnNum}`;
+        },
+        addToCart(item, e) {
+            this.cart.push(item);
+        },
     },
-  },
 };
 Vue.createApp(productApp).mount("#products");
