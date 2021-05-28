d1a206e6c60e7e8548d112816bf652f9bce49fdb
Abhishek Ghosh <ghosh.abhi91@gmail.com>
2021-05-28 17:27:53 +0530

    👌 Addsproducts as sub-component, ✔ Code cleanup

--- scripts/productList.js
+++ scripts/productList.js
@@ -5,10 +5,6 @@ const productApp = {
             btnColor: "btn-success",
             totalColor: "text-secondary",
             salesBtn: "btn-secondary",
-            borderStyle: {
-                borderRadius: "50%",
-                border: "3px solid darkgreen",
-            },
             max: 50,
             displayLabels: true,
             productsList: [],
@@ -39,10 +35,6 @@ const productApp = {
         },
     },
     methods: {
-        // currency(value) {
-        //     let fpnNum = Number.parseFloat(value).toFixed(2);
-        //     return `$${fpnNum}`;
-        // },
         addToCart(item, e) {
             this.cart.push(item);
             if (this.cartTotal >= 100) {
@@ -59,6 +51,42 @@ const productApp = {
 };
 
 const App = Vue.createApp(productApp);
+
+App.component("product", {
+    data() {
+        return {
+            borderStyle: {
+                borderRadius: "20%",
+                border: "3px solid",
+            },
+        };
+    },
+    props: ["item", "k", "i", "displayLabels"],
+    emits: ["addToCart"],
+    template: `<hr v-if="k!=0">
+    <h3 class="text-info">{{k + 1}}.</h3>
+    <div id="display-list" class="row d-flex mb-3 align-items-center">
+        <div class="col-1 m-auto">
+            <button class="btn btn-success" :style="borderStyle" @click="$emit('addToCart', item, $event)">
+                +
+            </button>
+        </div>
+        <div class="col-sm-4">
+            <img class="img-fluid d-block" :src="item.image" :alt="item.image_title">
+        </div>
+        <div class="col">
+            <h3 class="text-info">{{item.id}} | {{item.name}}</h3>
+            <p class="mb-0">{{item.description}}</p>
+            <div class="h5 float-right">
+                <span class="badge bg-success" v-if="item.price>=90 && displayLabels">premier</span>
+                <span class="badge bg-primary"
+                    v-else-if="item.price<90 && item.price>10 && displayLabels">value</span>
+                <span class="badge bg-danger" v-else-if="item.price<=10 && displayLabels">sale</span>
+                <curr :amt="item.price"></curr>
+            </div>
+        </div>
+    </div>`,
+});
 
 App.component("curr", {
     props: ["amt"],
