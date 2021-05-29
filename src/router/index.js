de2547bae291bbc700fe3ce2a82cb267039a6539
Abhishek Ghosh <ghosh.abhi91@gmail.com>
2021-05-29 19:00:30 +0530

    ➕ Initializing vue app

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+// import Checkout from '../views/Checkout.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
