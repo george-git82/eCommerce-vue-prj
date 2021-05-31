d921d160d153fb4e1e360908742cd69e9d562635
Abhishek Ghosh <ghosh.abhi91@gmail.com>
2021-05-31 17:08:26 +0530

    🐱‍👤 Moves navigation components and style to Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,34 @@
 <template>
-  <div id="nav" class="navbar navbar-light" style="background-color: #e3f2fd">
-    <router-link to="/"><fa icon="home" /> Home</router-link> |
-    <router-link to="/checkout"
-      ><fa icon="address-card" /> Checkout</router-link
-    >
+  <Navbar :cart="cart" />
+  <div class="container">
+    <router-view :cart="cart" @add-to-cart="addToCart" />
   </div>
-  <div class="container"><router-view /></div>
   <hr />
   <Footer />
 </template>
 
 <script>
 import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar'
+
 export default {
-  components: { Footer }
+  name: 'App',
+  data: function () {
+    return {
+      cart: [],
+      displayCart: false
+    }
+  },
+  components: { Footer, Navbar },
+  methods: {
+    addToCart(item, e) {
+      this.cart.push(item)
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 $primary: #6f42c1;
 @import 'node_modules/bootstrap/scss/bootstrap';
-
-.dropdown-clip {
-  overflow: hidden;
-}
-
-.dropdown-enter-active,
-.dropdown-leave-active {
-  transition: all 0.5s ease-in-out;
-  transform: auto;
-}
-
-.dropdown-enter-from,
-.dropdown-leave-to {
-  opacity: 0;
-  transform: translateY(-300px);
-}
 </style>
